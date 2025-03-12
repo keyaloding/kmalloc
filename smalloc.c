ea719930ecf142966e5668726827701228217ded
Keya Loding <loding.keya@gmail.com>
2025-03-12 12:21:16 -0500

    attempted `free()`

--- smalloc.c
+++ smalloc.c
@@ -1,30 +1,8 @@
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/mman.h>
 #include "smalloc.h"
 
-typedef struct free_block free_block;
-struct free_block {
-  int size, allocated;
-  free_block *prev, *next;
-};
+mem_block *head;
+void *heap_address;
 
-free_block *head=NULL;
-void *heap_address=NULL;
-
-/*
- * my_init() is called one time by the application program to to perform any
- * necessary initializations, such as allocating the initial heap area.
- * size_of_region is the number of bytes that you should request from the OS using
- * mmap().
- * Note that you need to round up this amount so that you request memory in
- * units of the page size, which is defined as 4096 Bytes in this project.
- */
 int my_init(int size_of_region) {
   if (size_of_region % 4096) {
     size_of_region += 4096 - (size_of_region % 4096);
@@ -36,34 +14,28 @@ int my_init(int size_of_region) {
     return -1;
   }
   heap_address = heap;
-  head = (free_block*)heap;
+  head = (mem_block*)heap;
   head->size = size_of_region;
   head->allocated = 0;
   head->prev = NULL; head->next = NULL;
   return 0;
 }
 
-/*
- * smalloc() takes as input the size in bytes of the payload to be allocated and
- * returns a pointer to the start of the payload. The function returns NULL if
- * there is not enough contiguous free space within the memory allocated
- * by my_init() to satisfy this request.
- */
-void* smalloc(int size_of_payload, Malloc_Status *status) {
+void* smalloc(int size_of_payload, Malloc_Status* status) {
   if (size_of_payload < 0) {
     status->success = 0;
     status->payload_offset = -1;
     status->hops = 0;
     return NULL;
   }
-  int alloc_size = size_of_payload + 24, hops = 0;
+  int alloc_size = size_of_payload + sizeof(mem_block), hops = 0;
   if (alloc_size % 8) {
     alloc_size += 8 - (alloc_size % 8);
   }
-  free_block* block = head;
+  mem_block* block = head;
   while (block && block->size < alloc_size) {
-    block = block->next;
     hops++;
+    block = block->next;
   }
   if (!block) {
     status->success = 0;
@@ -73,10 +45,10 @@ void* smalloc(int size_of_payload, Malloc_Status *status) {
   }
   block->allocated = 1;
   int bytes_left = block->size - alloc_size;
-  void* payload_start = (void *)(block + 1);
-  if (bytes_left >= 24) {
-    void *head_ptr = (void *)(block) + alloc_size;
-    free_block* new_block = (free_block*)head_ptr;
+  void* payload_start = (void*)(block + 1);
+  if (bytes_left >= sizeof(mem_block)) {
+    void *head_ptr = (void*)(block) + alloc_size;
+    mem_block* new_block = (mem_block*)head_ptr;
     new_block->size = bytes_left;
     new_block->allocated = 0;
     if (block->prev) {
@@ -106,40 +78,60 @@ void* smalloc(int size_of_payload, Malloc_Status *status) {
   return payload_start;
 }
 
-/*
- * sfree() frees the target block. "ptr" points to the start of the payload.
- * NOTE: "ptr" points to the start of the payload, rather than the block (header).
- */
-void sfree(void *ptr) {
-  free_block* block = (free_block*)(ptr - 24);
-  // printf("payload address: %p\n", ptr);
-  // printf("block size: %d\n", block->size);
-  block->allocated = 0;
-  // printf("--1--\n");
-  // if (!block->prev && block->next && !block->next->allocated) {
-  //   block->size += block->next->size;
-  //   block->next = block->next->next;
-  // } else if (block->prev && !block->next && !block->prev->allocated) {
-  //   block->prev->size += block->size;
-  //   block->prev->next = block->next;
-  // } else if (block->prev && block->next) {
-  //   if (!block->next->allocated) {
-  //     block->size += block->next->size;
-  //     block->next = block->next->next;
-  //   }
-  //   if (!block->prev->allocated) {
-  //     block->prev->size += block->size;
-  //     block->prev->next = block->next;
-  //   }
-  // }
-  if (block->next) {
-    if (block->next->allocated) {
+void sfree(void* ptr) {
+  printf("ptr at %p\n", ptr);
+  mem_block* alloc_block = (mem_block*)(ptr - 24);
+  printf("alloc_block at %p\n", alloc_block);
+  // printf("ptr - alloc_block = %ld\n", (uintptr_t)ptr - (uintptr_t)alloc_block);
+  alloc_block->allocated = 0;
+  mem_block *right_block = head, *left_block = NULL;
+  while (right_block && right_block < alloc_block) {
+    right_block = right_block->next;
+    if (!left_block) left_block = head;
+    else left_block = left_block->next;
+  }
+  if ((!left_block || (left_block && left_block->allocated)) &&
+      (!right_block) || (right_block && right_block->allocated)) {
+    // Case 1: No coalescing
+    if (left_block) {
+      left_block->next = alloc_block;
+    } else {
+      head = alloc_block;
+    }
+    alloc_block->prev = left_block;
+    if (right_block) {
+      right_block->prev = alloc_block;
+    } 
+    alloc_block->next = right_block;
+  } else if (1) {
+    // Case 2: Coalesce with right block
+    if (left_block) {
+      left_block->next = alloc_block;
+      alloc_block->prev = left_block;
+      alloc_block->next = right_block->next;
+      alloc_block->next->prev = alloc_block;
+    } else {
 
     }
+  } else if (1) {
+    // Case 3: Coalesce with left block
+    left_block->size += alloc_block->size;
+  } else {
+    // Case 4: Coalesce all 3 blocks
+    left_block->size += alloc_block->size + right_block->size;
+    left_block->next = right_block->next;
+    if (left_block->next) {
+      left_block->next->prev = left_block;
+    }
   }
-  if (block->prev) {
-    if (block->prev->allocated) {
-
+  if (left_block + left_block->size / 24 == alloc_block) {
+    left_block->size += alloc_block->size;
+  } else {
+    left_block->next = alloc_block;
+    alloc_block->prev = left_block;
+    if (right_block) {
+      right_block->prev = alloc_block;
+      alloc_block->next = right_block;
     }
   }
 }
--- smalloc.h
+++ smalloc.h
@@ -1,21 +1,65 @@
 #ifndef _smalloc_H_
 #define _smalloc_H_
 
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
 
+/* Represents the header of an allocated/free memory block. The format in
+ * memory is as follows: 
+ * - Block size (int, 4 bytes)
+ * - Allocated (int, 4 bytes): 0 if free, 1 if allocated
+ * - Next free block (pointer, 8 bytes)
+ * - Previous free block (pointer, 8 bytes)
+ * - Payload (allocated block only)
+ * - Padding (only if extra bytes are needed to enforce 8-byte alignment)
+ */
+typedef struct mem_block {
+  int size, allocated;
+  mem_block *prev, *next;
+} mem_block;
 
+/* Gives information about the status of the `malloc()` call*/
 typedef struct Malloc_Status{
-    int success;            // 1 if success. 0 if failed.
-    int payload_offset;     // Offset of the payload in the heap (in Bytes). 
-                            //   This should be (unsigned long)payload_pointer - (unsigned long)start_of_heap. 
-                            //   If malloc failed, then this should be -1.
-    int hops;               // Number of hops it takes to find the first-fit block.
-                            //   e.g. hops = 0 if the first free block (i.e. the head) of the free list satisfies.
-                            //        hops = 1 if the second free block of the free list satisfies.
-                            //   hops = -1 if malloc failed.
+  /* 0 if malloc fails, 1 if successful.*/
+  int success;
+  
+  /* Offset of the payload in the heap (in bytes). This is equal to
+   * (unsigned long)payload_pointer - (unsigned long)heap_start_address.
+   * If malloc fails, this is -1.
+   */
+  int payload_offset;
+
+  /* Number of hops it takes to find the first-fit block.
+   * e.g. hops = 0 if the first free block (the head) of the free list can fit
+   * the paylod. hops = 1 if the second free block can fit the payload.
+   * hops = -1 if malloc fails
+   */
+  int hops;
 } Malloc_Status;
 
+/*
+ * Called one time by the application program to allocate the initial heap area
+ * and set global variables `head` and `heap_address`. The size of the heap area
+ * must be a multiple of the page size, 4096.
+ */
 int my_init(int size_of_region);
+
+/*
+ * Returns a pointer to the start of the payload or NULL if there is not enough
+ * contiguous free space within the memory allocated by my_init() to satisfy
+ * the request.
+ */
 void *smalloc(int size_of_payload, Malloc_Status* status);
+
+/*
+ * Frees the target block. `ptr` points to the start of the payload.
+ */
 void sfree(void *ptr);
 
 #endif
